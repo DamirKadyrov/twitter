fb5b65be764c45b5de0902448dc50328599847ec
Damir <kadyrov27@gmail.com>
2014-12-15 14:17:45 +0000

    finish project

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,12 +18,12 @@ class UsersController < ApplicationController
   end
   
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      sign_in user
-      redirect_back_or user
+    @user = User.new(user_params)
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
-      flash.now[:error] = 'Invalid email/password combination'
       render 'new'
     end
   end
@@ -41,7 +41,19 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-  
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User deleted."
